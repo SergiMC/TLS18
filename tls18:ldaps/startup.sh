92eb1a53caffb072ae74d610bd8e407f98aca426
dsoler <dsoler700@gmail.com>
2019-03-27 11:32:27 +0100

    files news

--- tls18:ldaps/startup.sh
+++ tls18:ldaps/startup.sh
@@ -4,5 +4,6 @@
 # -------------------------------------
 
 /opt/docker/install.sh && echo "Install Ok"
-/sbin/slapd -d0  && echo "slapd Ok"
+/sbin/slapd -d0 -u ldap -h "ldap:/// ldaps:/// ldapi:///" && echo "slapd Ok"
+##està copiat del slapd
 
